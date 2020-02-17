168bcdeaaf8659966412de25eafb3bb53cb95ae0
Nathanael Bowles <bowlesna@oregonstate.edu>
2020-02-17 11:44:57 -0800

    included various functions

---
+++ R/coordinates_to_xbins.R
@@ -0,0 +1,53 @@
+# Coordinates_to_xbins
+# Takes a data frame and a number of bins [must be greater than 1 (no error checking)]
+# Outputs a data frame with the type of kernel counted up per bin
+
+coordinates_to_xbins <- function(input_df, num_bins) {
+  
+  
+  #calculate amount of length per bin
+  xmax = max(input_df$x)
+  xmin = min(input_df$x)
+  bins = (xmax-xmin)/num_bins
+  
+  #split data into bins by brute force
+  
+  #make new data frame for column by column
+  final_df = data.frame(bins= seq(xmin,xmax,bins))
+  WT_df = data.frame(WT = c(1:(nrow(final_df)))*0)
+  GFP_df = data.frame(GFP = c(1:(nrow(final_df)))*0)
+  
+  
+  #merge function adds an extra column called "Row.names" so I remove them immedietely after... twice
+  final_df = merge(final_df, WT_df, by="row.names", sort=FALSE)
+    final_df = subset(final_df, select = -c(Row.names))
+  final_df = merge(final_df, GFP_df,by="row.names", sort=FALSE)
+    final_df = subset(final_df, select = -c(Row.names))
+  
+  #sequences for the for-loops that come next
+  sequence = seq(1,nrow(input_df)) 
+  sequence2 = seq(1,nrow(final_df)-1)
+  
+  #The brute force part, requires that the final_df is empty or it will double count
+  
+  for (val in sequence) {                                             #for each value
+    for(i in sequence2) {                                              #for each bin
+      
+      #if the value is in the bin, but not any bins before it
+      if ((input_df$x[val] >= final_df$bins[i]) && (input_df$x[val] < final_df$bins[i+1]) ) { 
+        if (input_df$type[val] == "WT") {                            #has to check if the kernel is WT or GFP then increments bin
+          final_df$WT[i] = final_df$WT[i] + 1                      
+        }               
+        else if (input_df$type[val] == "GFP") {
+          final_df$GFP[i] = final_df$GFP[i] + 1
+        }
+      }
+      
+    }
+  }
+  
+  #remove excess row
+  final_df = final_df[-c(num_bins+1),]
+  
+  return(final_df)
+}
